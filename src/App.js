d5c28ae3b9af5fe4eff8a7baa10ab4a3b7341165
Tze Jit Kho <tzejit.kho@portcast.io>
2024-02-16 18:19:08 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Menu from './components/Menu';
+import Analytics from './components/Analytics';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+
+const theme = createTheme({ 
+    palette: {  primary: {main: '#f2f2f2' },
+                yellow: {main: '#ffc000', transparent: '#ffc00033' }, 
+                grey: {main: '#252f3f'},
+                brown: {main: '#420D0D'}},
+    typography: {
+        button: {
+          textTransform: 'none',
+          flexDirection: "column"
+        }
+      }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <Box>
+            <Typography color='grey.main' variant='h5' fontWeight='fontWeightMedium' margin='1em'>Welcome to InSight!</Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={2} sx={{position: 'sticky', top: 0, alignSelf: 'start', marginLeft:'1vw'}}>
+                    <Menu />
+                </Grid>
+                <Grid item xs='auto'>
+                    <Divider orientation="vertical"/>
+                </Grid>
+                <Grid item xs={9}>
+                    <Analytics/>
+                </Grid>
+            </Grid>
+        </Box>
+    </ThemeProvider>
+
   );
 }
 
